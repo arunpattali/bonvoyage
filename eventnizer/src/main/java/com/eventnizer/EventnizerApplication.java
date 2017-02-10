91c17be751738b097972ac29e5dfbb3461c5e9cc
ram1ramji <ram1ramji@gmail.com>
2017-02-10 16:42:29 +0400

    Add files via upload

---
+++ eventnizer/src/main/java/com/eventnizer/EventnizerApplication.java
@@ -0,0 +1,12 @@
+package com.eventnizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventnizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventnizerApplication.class, args);
+	}
+}
